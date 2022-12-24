2b8ee80085ec9930005d3d0444309a5aada81cb8
alevcoder <alevcoder1@gmail.com>
2022-12-24 22:47:41 +0500

    skills model change

--- teamwork/skills/admin.py
+++ teamwork/skills/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 from teamwork.skills.models import Skills, Specialties
-
+from mptt.admin import DraggableMPTTAdmin
 # Register your models here.
 admin.site.register(Skills)
-admin.site.register(Specialties)
+
+@admin.register(Specialties)
+class SpecialtiesAdmin(DraggableMPTTAdmin):
+    mptt_level_indent = 20
--- teamwork/skills/models.py
+++ teamwork/skills/models.py
@@ -3,18 +3,20 @@ from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
 
 
-class Skills(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Specialties(MPTTModel):
     name_uz = models.CharField(max_length=100)
     name_ru = models.CharField(max_length=100)
     name_en = models.CharField(max_length=100)
-    parent = TreeForeignKey('self', on_delete=models.CASCADE, related_name='children')
+    parent = TreeForeignKey('self', on_delete=models.SET_NULL, related_name='children', null=True, blank=True)
 
     def __str__(self):
         return self.name_uz
+
+
+class Skills(models.Model):
+    special = models.ManyToManyField(Specialties, related_name="special_skills", blank=True)
+    name = models.CharField(max_length=100)
+    is_moderated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
